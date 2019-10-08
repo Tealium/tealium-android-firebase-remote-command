1bed2ffd55b2cca8c105a1e9ab80d364b857b82d
jameskeith <james.keith@tealium.com>
2019-10-08 10:04:38 -0700

    ExampleApp tidy

--- app/src/main/java/com/tealium/example/GamingActivity.java
+++ app/src/main/java/com/tealium/example/GamingActivity.java
@@ -95,6 +95,4 @@ public class GamingActivity extends AppCompatActivity implements View.OnClickLis
     private void showMessage(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
-
-
 }
