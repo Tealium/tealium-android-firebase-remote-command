6d7ce54242e17741e3d95a1ba6415016dc65bdc8
Karen Tamayo <karen.tamayo@tealium.com>
2020-10-21 20:43:53 -0700

    Correct import for constants

--- app/src/main/java/com/tealium/example/MainActivity.java
+++ app/src/main/java/com/tealium/example/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 import com.tealium.example.helper.TealiumHelper;
-import com.tealium.remotecommands.firebase.*;
+import com.tealium.remotecommands.firebase.FirebaseConstants;
 
 import java.util.HashMap;
 import java.util.Map;
